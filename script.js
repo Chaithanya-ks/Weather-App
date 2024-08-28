47d89278c78b3707af0ad03299088ec9e32379a4
Chaithanya-ks <chaithanyakedilaya@gmail.com>
2024-08-28 12:11:49 +0530

    first commit

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
 
 async function checkWeather(city) {
-    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`); 
    
     if(response.status == 404){
         document.querySelector(".error").style.display = "block";
-        document.querySelector(".weather").style.display = "none";
+        document.querySelector(".weather").style.display = "none";  
     }
     else{
         var data = await response.json();
